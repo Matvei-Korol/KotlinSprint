21f3f2d5f26aae3809afa54d8d3c24dcc1301544
menay <matvei.korol2000@gmail.com>
2025-06-17 18:51:02 +0300

    KS-1-3 solution of task 3

---
+++ src/main/kotlin/lesson_1/lesson_1_Task_3.kt
@@ -0,0 +1,20 @@
+package lesson_1
+
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println("Взлет ->>\n" +
+            "Год: $year \n" +
+            "Час: $hour \n" +
+            "Минута: $minute \n")
+
+    hour = 10
+    minute = 55
+
+    println("Посадка ->> \n" +
+            "Год: $year \n" +
+            "Час: $hour \n" +
+            "Минута: $minute \n")
+}
